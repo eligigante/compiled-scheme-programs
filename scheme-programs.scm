c8f90d8135f854673d13edf723d43113755819b9
Kibent123 <2224372@slu.edu.ph>
2023-12-17 22:45:29 +0800

    issue

--- scheme-programs.scm
+++ scheme-programs.scm
@@ -55,7 +55,7 @@
   (display "Enter student's ID: ")
   (let ((studentID (read)))
     (display "Enter student's name: ")
-    (let ((studentsName (read)))
+    (let ((studentsName (read)));studentName instead of studentsName
       (display "Enter student's first quarter grade: ")
       (let ((firstQuarter (read)))
         (display "Enter student's second quarter grade: ")
@@ -64,7 +64,7 @@
           (let ((thirdQuarter (read)))
             (display "Enter student's fourth quarter grade: ")
             (let ((fourthQuarter (read)))
-              (let ((sum (+ firstQuarter secondQuarter thirdQuarter fourthQuarter)))
+              (let ((sum (- firstQuarter secondQuarter thirdQuarter fourthQuarter)));(-) should be (+)
                 (let ((final-grade (/ sum 0))) ;(let ((final-grade (/ sum 40))) 
                   (display "Student's final grade: ")
                   (roundoff final-grade)
